ddca884665dfbc03d7609498e3038579076101d2
cherylafitz <cherylafitzgerald@gmail.com>
2015-09-12 12:49:21 -0700

    setup

---
+++ api/controllers/YelpController.js
@@ -0,0 +1,13 @@
+/**
+ * YelpController
+ *
+ * @description :: Server-side logic for managing yelps
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+var request = require('request');
+
+module.exports = {
+
+};
+
